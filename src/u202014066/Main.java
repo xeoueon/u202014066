85f4c4e3ebda62ccebcac843646d009ae2ca2cce
Kim, Seoyeon <ssyy9166@gamil.com>
2024-04-24 02:58:41 +0900

    Three

--- src/u202014066/Main.java
+++ src/u202014066/Main.java
@@ -3,7 +3,7 @@ package u202014066;
 public class Main {
 
   public static void main(String[] args) {
-    Two.print();
     System.out.println("one 202014066");
+    Three.print();
   }
 }
---
+++ src/u202014066/Three.java
@@ -0,0 +1,8 @@
+package u202014066;
+
+public class Three {
+
+  public static void print() {
+    System.out.println("three");
+  }
+}
